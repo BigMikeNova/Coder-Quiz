b21aa6736d3def9aa43cd674b29f634af770ff66
BigMikeNova <125073452+BigMikeNova@users.noreply.github.com>
2023-03-19 16:36:59 -0400

    added sound functionality on incorrect and correct answers to js, added sound link to html

--- assets/script.js
+++ assets/script.js
@@ -22,33 +22,30 @@ const goBackBtn = document.querySelector("#goback");
 const clearScrBtn = document.querySelector("#clearscores");
 const viewScrBtn = document.querySelector("#view-scores");
 
+
+
 const questions = [ 
-    {
-        // question 0
+    { // question 0
         question: "Commonly used data types do NOT include:",
         answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
         correctAnswer: "2"
     },
-    {
-        // question 1
+    { // question 1
         question: "The condition in an if / else statement is enclosed within ____.",
         answers: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
         correctAnswer: "1"
     },
-    {
-        // question 2
+    { // question 2
         question: "Arrays in Javascript can be used to store ____.",
         answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
         correctAnswer: "3"
     },
-    {
-        // question 3
+    {// question 3
         question: "String values must be enclosed within ____ when being assigned to variables.",
         answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
         correctAnswer: "2"
     },
-    {
-        // question 4
+    {// question 4
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         answers: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
         correctAnswer: "3"
@@ -100,18 +97,13 @@ function checkAnswer(event) {
     }, 1000);
 
     if (questions[questionCount].correctAnswer === event.target.value) {
+        document.getElementById('correct-sound').play();
         p.textContent = "Correct!";
-        function myPlay() {
-            var a = document.getElementById('correct');
-            a.play();
     } else if (questions[questionCount].correctAnswer !== event.target.value) {
+        document.getElementById('incorrect-sound').play();
         secondsLeft = secondsLeft - 10;
         p.textContent = "Wrong!";
-        function myPlay() {
-            var b = document.getElementById('incorrect');
-            b.play();
     }
-
     if (questionCount < questions.length) {
         questionCount++;
     }
